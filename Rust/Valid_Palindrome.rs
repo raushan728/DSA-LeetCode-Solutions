58371208d99d01217d2f69019b7ccd6e8ada58e6
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-08-25 22:33:47 +0530

    Rust Valid Palindrome

---
+++ Rust/Valid_Palindrome.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let s = s.chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect::<Vec<_>>();
+        let (mut l, mut r) = (0, s.len().saturating_sub(1));
+        while l < r {
+            if s[l] != s[r] {
+                return false;
+            }
+            l += 1;
+            r -= 1;
+        }
+        true
+    }
+}
