3e47df59affb05ba0373e15f5069181c3514a5e8
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-11 17:27:17 +0530

    Majority Element in C++

---
+++ C++/Majority_Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans = 0;
+        for (int num : nums) {
+            if (freq == 0) {
+                ans = num;
+            }
+            if (num == ans) {
+                freq++;
+            } else
+                freq--;
+        }
+        return ans;
+    }
+};
