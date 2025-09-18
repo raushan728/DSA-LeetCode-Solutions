c08212e5f4e945beb08b2ce0f25505ffb3432878
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-19 00:32:48 +0530

    Single Number

--- Cpp/136_Single_Number.cpp
+++ Cpp/136_Single_Number.cpp
@@ -1,13 +1,9 @@
-class Solution
-{
+class Solution {
 public:
-    int singleNumber(vector<int> &nums)
-    {
+    int singleNumber(vector<int>& nums) {
         int result = 0;
         for (int i : nums)
-        {
             result ^= i;
-        }
         return result;
     }
 };
