b3df0ad987fe40100bc69ab2f6d7567f085f6857
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-19 01:22:06 +0530

    Shortest Unsorted Continuous Subarray

--- Cpp/581_Shortest_Unsorted_Continuous_Subarray.cpp
+++ Cpp/581_Shortest_Unsorted_Continuous_Subarray.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int st = 0, end = nums.size() - 1;
-        while (st < end && nums[st] <= nums[st + 1]) {
+        while (st < end && nums[st] <= nums[st + 1])
             st++;
-        }
-        if (st == nums.size() - 1) {
+        if (st == nums.size() - 1)
             return 0;
-        }
-        while (end > 0 && nums[end] >= nums[end - 1]) {
+        while (end > 0 && nums[end - 1] <= nums[end])
             end--;
-        }
         int submin = *min_element(nums.begin() + st, nums.begin() + end + 1);
         int submax = *max_element(nums.begin() + st, nums.begin() + end + 1);
-        while (st > 0 && nums[st - 1] > submin) {
+        while (st > 0 && nums[st - 1] > submin)
             st--;
-        }
-        while (end < nums.size() - 1 && nums[end + 1] < submax) {
+        while (end < nums.size() - 1 && nums[end + 1] < submax)
             end++;
-        }
         return end - st + 1;
     }
 };
