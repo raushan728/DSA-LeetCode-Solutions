990e90786a84ba98a71f7c35ae1ebca1491f4fd0
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-18 10:09:15 +0530

    Updated

--- C++/581_Shortest_Unsorted_Continuous_Subarray.cpp
+++ C++/581_Shortest_Unsorted_Continuous_Subarray.cpp
@@ -1,25 +1,24 @@
 class Solution {
-    public:
-        int findUnsortedSubarray(vector<int>& nums) {
-            int n = nums.size();
-            int start = 0, end = n - 1;
-            while (start < n - 1 && nums[start] <= nums[start + 1]) {
-                start++;
-            }
-            if (start == n - 1) {
-                return 0;
-            }
-            while (end > 0 && nums[end] >= nums[end - 1]) {
-                end--;
-            }
-            int submin = *min_element(nums.begin() + start, nums.begin() + end + 1);
-            int submax = *max_element(nums.begin() + start, nums.begin() + end + 1);
-            while (start > 0 && nums[start - 1] > submin) {
-                start--;
-            }
-            while (end < n - 1 && nums[end + 1] < submax) {
-                end++;
-            }
-            return end - start + 1;
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int st = 0, end = nums.size() - 1;
+        while (st < end && nums[st] <= nums[st + 1]) {
+            st++;
         }
-    };
+        if (st == nums.size() - 1) {
+            return 0;
+        }
+        while (end > 0 && nums[end] >= nums[end - 1]) {
+            end--;
+        }
+        int submin = *min_element(nums.begin() + st, nums.begin() + end + 1);
+        int submax = *max_element(nums.begin() + st, nums.begin() + end + 1);
+        while (st > 0 && nums[st - 1] > submin) {
+            st--;
+        }
+        while (end < nums.size() - 1 && nums[end + 1] < submax) {
+            end++;
+        }
+        return end - st + 1;
+    }
+};
