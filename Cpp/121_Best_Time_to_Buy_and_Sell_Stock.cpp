bdedd6b56e0b4d6f76c3381c7b9660e4f6030772
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-21 09:42:18 +0530

    Best Time to Buy and Sell Stock

--- Cpp/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Cpp/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,25 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int> &prices)
-    {
-        int min_price = INT_MAX;
-        int max_profit = 0;
-        for (int price : prices)
-        {
-            if (price < min_price)
-            {
-                min_price = price;
-            }
-            else
-            {
-                int profit = price - min_price;
-                if (profit > max_profit)
-                {
-                    max_profit = profit;
-                }
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for (int i : prices) {
+            if (minPrice > i)
+                minPrice = i;
+            else {
+                int profit = i - minPrice;
+                if (profit > maxProfit)
+                    maxProfit = profit;
             }
         }
-        return max_profit;
+        return maxProfit;
     }
 };
