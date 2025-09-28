e0e0417e3f876cc270ace10f2ef6da8bfeee9534
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-09-29 00:02:49 +0530

    Top K Frequent Elements

--- Cpp/Top_K_Frequent_Elements.cpp
+++ Cpp/Top_K_Frequent_Elements.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freqMap;
-        for (int i : nums) {
-            freqMap[i]++;
-        }
+        for (int num : nums)
+            freqMap[num]++;
         vector<vector<int>> bucket(nums.size() + 1);
-        for (auto const& [num, freq] : freqMap) {
+        for (auto const& [num, freq] : freqMap)
             bucket[freq].push_back(num);
-        }
         vector<int> result;
         for (int i = bucket.size() - 1; i >= 0 && result.size() < k; i--) {
             if (!bucket[i].empty()) {
-                for (int nums : bucket[i]) {
-                    result.push_back(nums);
-                    if (result.size() == k) {
-                        return result;
-                    }
-                }
+                for (int i : bucket[i])
+                    result.push_back(i);
+                if (result.size() == k)
+                    return result;
             }
         }
         return result;
