0c4047b9e1292b8aa10081839abf4b67031764d8
Raushan kumar <raushansinghrajpoot687@gmail.com>
2025-12-26 13:45:17 +0000

    feat: Add C++ and Rust solutions for Maximum Depth of Binary Tree and update README.

---
+++ Rust/Maximum_Depth_of_Binary_Tree.rs
@@ -0,0 +1,17 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp::max;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(node) => {
+                let curr_node = node.borrow();
+                let left_height = Self::max_depth(curr_node.left.clone());
+                let right_height = Self::max_depth(curr_node.right.clone());
+                1 + max(left_height, right_height)
+            }
+        }
+    }
+}
